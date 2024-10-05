17f5f47bdb27e844d01d520bc7c741415391d722
Gustave Monce <gustave.monce@outlook.com>
2024-10-05 20:19:52 +0200

    Firmware: Update FFU building scripts

--- tools/Firmware/build_now.cmd
+++ tools/Firmware/build_now.cmd
@@ -1,6 +1,49 @@
 @echo off
 
 REM X23-81973_26100.1742.240906-0331.ge_release_svc_refresh_CLIENT_CONSUMER_A64FRE_en-us.iso
+REM Index 1: Home
+REM Index 2: Home Single Language
 REM Index 3: Pro
 
-%PROCESSOR_ARCHITECTURE%\FirmwareGen.exe -w D: -i 3 -v 10.0.26100.1742 -o "%CD%\FFUs" -d "%CD%\..\.." -p %CD%\DeviceProfiles\EpsilonHalfSplit128GB.xml
+set WINDOWS_ISO_IMAGE=%USERPROFILE%\Downloads\X23-81973_26100.1742.240906-0331.ge_release_svc_refresh_CLIENT_CONSUMER_A64FRE_en-us.iso
+set WINDOWS_ISO_IMAGE_INDEX=3
+set WINDOWS_ISO_IMAGE_VERSION=10.0.26100.1742
+
+REM Mount ISO file
+powershell -command "(Mount-DiskImage -ImagePath '%WINDOWS_ISO_IMAGE%' | Get-Volume).DriveLetter" > tmpFile
+set /p DriveLetter= < tmpFile
+del tmpFile
+
+REM Generate Base Virtual Disk file
+REM Generate FFU Files
+
+REM Epsilon Half Split 128GB
+%PROCESSOR_ARCHITECTURE%\FirmwareGen.exe -w %DriveLetter%: -i %WINDOWS_ISO_IMAGE_INDEX% -v %WINDOWS_ISO_IMAGE_VERSION% -o "%CD%\FFUs" -d "%CD%\..\.." -p %CD%\DeviceProfiles\EpsilonHalfSplit128GB.xml
+call resign_ffu.cmd "%CD%\FFUs\OEMEP_128GB_HalfSplit.ffu"
+
+REM Epsilon Half Split 256GB
+%PROCESSOR_ARCHITECTURE%\FirmwareGen.exe -w %DriveLetter%: -i %WINDOWS_ISO_IMAGE_INDEX% -v %WINDOWS_ISO_IMAGE_VERSION% -o "%CD%\FFUs" -d "%CD%\..\.." -p %CD%\DeviceProfiles\EpsilonHalfSplit256GB.xml
+call resign_ffu.cmd "%CD%\FFUs\OEMEP_256GB_HalfSplit.ffu"
+
+REM Epsilon Maximized for Windows
+%PROCESSOR_ARCHITECTURE%\FirmwareGen.exe -w %DriveLetter%: -i %WINDOWS_ISO_IMAGE_INDEX% -v %WINDOWS_ISO_IMAGE_VERSION% -o "%CD%\FFUs" -d "%CD%\..\.." -p %CD%\DeviceProfiles\EpsilonMaximizedForWindows.xml
+call resign_ffu.cmd "%CD%\FFUs\OEMEP_MaximizedForWindows.ffu"
+
+REM Zeta Half Split 128GB
+%PROCESSOR_ARCHITECTURE%\FirmwareGen.exe -w %DriveLetter%: -i %WINDOWS_ISO_IMAGE_INDEX% -v %WINDOWS_ISO_IMAGE_VERSION% -o "%CD%\FFUs" -d "%CD%\..\.." -p %CD%\DeviceProfiles\ZetaHalfSplit128GB.xml
+call resign_ffu.cmd "%CD%\FFUs\OEMZE_128GB_HalfSplit.ffu"
+
+REM Zeta Half Split 256GB
+%PROCESSOR_ARCHITECTURE%\FirmwareGen.exe -w %DriveLetter%: -i %WINDOWS_ISO_IMAGE_INDEX% -v %WINDOWS_ISO_IMAGE_VERSION% -o "%CD%\FFUs" -d "%CD%\..\.." -p %CD%\DeviceProfiles\ZetaHalfSplit256GB.xml
+call resign_ffu.cmd "%CD%\FFUs\OEMZE_256GB_HalfSplit.ffu"
+
+REM Zeta Half Split 512GB
+REM %PROCESSOR_ARCHITECTURE%\FirmwareGen.exe -w %DriveLetter%: -i %WINDOWS_ISO_IMAGE_INDEX% -v %WINDOWS_ISO_IMAGE_VERSION% -o "%CD%\FFUs" -d "%CD%\..\.." -p %CD%\DeviceProfiles\ZetaHalfSplit512GB.xml
+REM call resign_ffu.cmd "%CD%\FFUs\OEMZE_512GB_HalfSplit.ffu"
+
+REM Zeta Maximized for Windows
+%PROCESSOR_ARCHITECTURE%\FirmwareGen.exe -w %DriveLetter%: -i %WINDOWS_ISO_IMAGE_INDEX% -v %WINDOWS_ISO_IMAGE_VERSION% -o "%CD%\FFUs" -d "%CD%\..\.." -p %CD%\DeviceProfiles\ZetaMaximizedForWindows.xml
+call resign_ffu.cmd "%CD%\FFUs\OEMZE_MaximizedForWindows.ffu"
+
+REM Unmount ISO image
+powershell -command "Dismount-DiskImage -ImagePath '%WINDOWS_ISO_IMAGE%'"
--- tools/Firmware/resign_ffu.cmd
+++ tools/Firmware/resign_ffu.cmd
@@ -1,7 +1,7 @@
 @echo off
 set SIGNING_CERT=\SecureBoot\Certs\private\OEMA0-RootPK.pfx
 set WPDK="%CD%\i386"
-set FFU="%CD%\FFUs\OEMEP_128GB_HalfSplit.ffu"
+set FFU=%1
 
 del SigningCab.cab
 del Flash.cat
@@ -9,10 +9,12 @@ del Flash.cat
 "%WPDK%\imageutility.exe" CreateSigningCab -FfuPath "%FFU%" -CabPath SigningCab.cab
 "%WPDK%\imageutility.exe" ExtractCatalog -CabPath SigningCab.cab -CatalogPath Flash.cat
 
-REM If %PROCESSOR_ARCHITECTURE%==AMD64, then it should be x64 not AMD64
-REM TODO: Fix above
+set WINDOWS_KIT_ARCH_FOLDER=%PROCESSOR_ARCHITECTURE%
+if %WINDOWS_KIT_ARCH_FOLDER%==AND64 (
+    set WINDOWS_KIT_ARCH_FOLDER=x64
+)
 
-"%ProgramFiles(x86)%\Windows Kits\10\bin\10.0.22621.0\%PROCESSOR_ARCHITECTURE%\signtool.exe" sign /td sha256 /fd sha256 /f "%SIGNING_CERT%" /p "" /tr http://timestamp.digicert.com Flash.cat
+"%ProgramFiles(x86)%\Windows Kits\10\bin\10.0.22621.0\%WINDOWS_KIT_ARCH_FOLDER%\signtool.exe" sign /td sha256 /fd sha256 /f "%SIGNING_CERT%" /p "" /tr http://timestamp.digicert.com Flash.cat
 
 "%WPDK%\imageutility.exe" InjectCatalog -CabPath SigningCab.cab -CatalogPath Flash.cat
 "%WPDK%\imageutility.nosigcheck.exe" ProcessSigningCab -FfuPath "%FFU%" -CabPath SigningCab.cab
